a625f78f88f62fd39c5bd0f245752025442ad705
Yash Sharma <yashsharma170898@gmail.com>
2024-02-08 22:34:46 +0530

    created upps script

--- contracts/script/Counter.s.sol
+++ contracts/script/Counter.s.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {RebalancerModule} from "../src/RebalancerModule.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        address proxy = Upgrades.deployUUPSProxy(
+    "RebalancerModule.sol",
+    abi.encodeCall(RebalancerModule.initialize, ("arguments for the initialize function"))
+);
     }
 }
--- contracts/src/RebalancerModule.sol
+++ contracts/src/RebalancerModule.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
 contract RebalancerModule {
     string public constant NAME = "Rebalancer Module";
     string public constant VERSION = "0.1.0";
